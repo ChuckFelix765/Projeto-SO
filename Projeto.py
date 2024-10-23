c9a30357cd50283797e7961db3f5cb50e40dd2c3
Mr.Felix <104478664+ChuckFelix765@users.noreply.github.com>
2024-10-23 08:23:29 -0300

    Add files via upload

--- Projeto.py
+++ Projeto.py
@@ -1,6 +1,7 @@
 f = open("Teste.txt","r")
 
 l = []
+dit = []
 
 for x in f:
     res = [i for parte in x.split(',') for i in parte.split()]
@@ -8,6 +9,14 @@ for x in f:
 
 print(l)
 
+for i in range(len(l)):
+    dic = {
+        l[i][0] : i+1
+    }
+    dit.append(dic)
+
+
+print(dit[0])
 
 for i in range(len(l)):
     for j in range(len(l[i])):
